6c0ad6f08f2eb9f9bc34837479134b2751d129d6
SandyN90 <sandeepkumarnavik5@gmail.com>
2023-02-11 18:32:09 +0530

    Added setup

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const server = express();
+
+const middleware = (req, res, next) => {
+    console.log("this is middleware");
+    next();
+}
+
+server.get('/', (req, res) => {
+    console.log("This port is running on port number 3000");
+    res.send('This is home page');
+});
+
+server.get('/aboutus', (req, res) => {
+    console.log("This port is running on port number 3000");
+    res.send('This is about us page');
+});
+
+server.get('/contact', (req, res) => {
+    console.log("This port is running on port number 3000");
+    res.send('This is contact us page');
+});
+
+server.get('/sign-in', middleware, (req, res) => {
+    console.log("This port is running on port number 3000");
+    res.send('This is sign in page');
+});
+server.listen(3000);

52e249a9c9f6da3e8aaf316b2c3b22993c913d43
SandyN90 <sandeepkumarnavik5@gmail.com>
2023-02-11 17:33:28 +0530

    Added server

---
+++ server/database/connection.js
@@ -0,0 +1,12 @@
+// sandy
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(DB).then(() => {
+    console.log("connection successful");
+}).catch(() => {
+    console.log('no connection');
+});

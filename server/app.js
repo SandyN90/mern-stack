63af9246466a599a7e48f7371e6f72ead10b75c3
SandyN90 <sandeepkumarnavik5@gmail.com>
2023-02-12 09:00:37 +0530

    Added dotenv library to the project

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('./database/connection')
 const server = express();
 
 const middleware = (req, res, next) => {
--- server/database/connection.js
+++ server/database/connection.js
@@ -1,7 +1,7 @@
 // sandy
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.env' });
 const DB = process.env.DATABASE;
 
 mongoose.set('strictQuery', false);
